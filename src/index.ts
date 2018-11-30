b00c65639bfecb0c73131c6d2f9056bf0e631b92
Alexander Kachkaev <alexander@kachkaev.ru>
2018-11-30 23:37:37 +0000

    Revert index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloLink, Observable, Operation } from 'apollo-link';
+const sha256 = require('hash.js/lib/hash/sha/256');
 import { print } from 'graphql/language/printer';
 import {
   DefinitionNode,
@@ -26,7 +27,9 @@ namespace PersistedQueryLink {
 }
 
 export const defaultGenerateHash = (query: DocumentNode): string =>
-  sha256(print(query));
+  sha256()
+    .update(print(query))
+    .digest('hex');
 
 export const defaultOptions = {
   generateHash: defaultGenerateHash,
@@ -68,9 +71,8 @@ function operationIsQuery(operation: Operation) {
 
 const { hasOwnProperty } = Object.prototype;
 const hashesKeyString = '__createPersistedQueryLink_hashes';
-const hashesKey = typeof Symbol === 'function'
-  ? Symbol.for(hashesKeyString)
-  : hashesKeyString;
+const hashesKey =
+  typeof Symbol === 'function' ? Symbol.for(hashesKeyString) : hashesKeyString;
 let nextHashesChildKey = 0;
 
 export const createPersistedQueryLink = (
@@ -85,7 +87,7 @@ export const createPersistedQueryLink = (
 
   const hashesChildKey = 'forLink' + nextHashesChildKey++;
   function getQueryHash(query: DocumentNode): string {
-    if (!query || typeof query !== "object") {
+    if (!query || typeof query !== 'object') {
       // If the query is not an object, we won't be able to store its hash as
       // a property of query[hashesKey], so we let generateHash(query) decide
       // what to do with the bogus query.
@@ -100,7 +102,7 @@ export const createPersistedQueryLink = (
     const hashes = (query as any)[hashesKey];
     return hasOwnProperty.call(hashes, hashesChildKey)
       ? hashes[hashesChildKey]
-      : hashes[hashesChildKey] = generateHash(query);
+      : (hashes[hashesChildKey] = generateHash(query));
   }
 
   return new ApolloLink((operation, forward) => {
